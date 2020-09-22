65b390c76998d5920b89a352eb0491dba3f6f8f7
Steve Grise <steve@vertex3.com>
2020-09-22 20:23:27 +0000

    Revert "Revert "Production changes applied""
    This reverts commit 14b14f41851ecefbd342f5b0ea990d719a85fbef.

---
+++ ETL/Update_New_Production.bat
@@ -0,0 +1,2 @@
+call C:\Apps\FME\fme.exe C:\Apps\Gizinta\gseUW\ETL\fme\uw_update_new_cadsde.fmw
+call C:\Apps\FME\fme.exe C:\Apps\Gizinta\gseUW\ETL\fme\mapdata-to-newmapdata.fmw
---
+++ ETL/Update_Old_CadSde.bat
@@ -0,0 +1,2 @@
+call C:\Apps\FME\fme.exe C:\Apps\Gizinta\gseUW\ETL\fme\uw_update_FacilityTable_cadsde.fmw
+call C:\Apps\FME\fme.exe C:\Apps\Gizinta\gseUW\ETL\fme\uw_update_old_cadsde.fmw
